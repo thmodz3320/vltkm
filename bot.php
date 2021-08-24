bc30d4728ebb94a4d563c6fc4c8e0fd250f9763d
thmodz3320 <thmodz3320@gmail.com>
2021-08-24 20:10:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,91 +1,67 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
 
-$path = "https://api.telegram.org/bot1774522961:AAFkfzcSEAaVkKxtT0f_g3AIfRSd3Vb4Ucg";
+    date_default_timezone_set("Asia/kolkata");
+    //Data From Webhook
+    $content = file_get_contents("php://input");
+    $update = json_decode($content, true);
+    $chat_id = $update["message"]["chat"]["id"];
+    $message = $update["message"]["text"];
+    $message_id = $update["message"]["message_id"];
+    $id = $update["message"]["from"]["id"];
+    $username = $update["message"]["from"]["username"];
+    $firstname = $update["message"]["from"]["first_name"];
+    $chatname = $_ENV['CHAT']; 
+ /// for broadcasting in Channel
+$channel_id = "-100xxxxxxxxxx";
 
-$update = json_decode(file_get_contents("php://input") , true);
+/////////////////////////
 
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
-function ServerList($sv)
-{
-    switch ($sv)
-    {
-        case 'sg':
-            return 'https://nap.volamsaigon.mobi/config/Telegram/';
-        break;
-
-        case 'cc':
-            return 'https://nap.jxcucai.net/config/Telegram/';
-        break;
-
-        case 'hm':
-            return 'https://nap.jxhoanmy.com/config/Telegram/';
-        break;
+    //Extact match Commands
+    if($message == "/start"){
+        send_message($chat_id,$message_id, "Hey $firstname \nUse /cmds to view commands \n$chatname");
     }
-}
-function ServerListName($sv)
-{
-    switch ($sv)
-    {
-        case 'sg':
-            return "Sài Gòn";
-        break;
 
-        case 'cc':
-            return "Củ Cải";
-        break;
-
-        case 'hm':
-            return "Hoàn Mỹ";
-        break;
-		
-		default:
-		return false;
-		break;
-    }
-}
 $SV = explode(" ", $message)[1];
 $CauLenh = explode(" ", $message)[2];
 if (strpos($message, "/help") === 0)
 {
-    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=⭕️ /rsmoc tensvviettat tentk");
-    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=⭕️ /online tensvviettat");
-    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=⭕️ /allonline tensvviettat");
+    send_message("⭕️ /rsmoc tensvviettat tentk");
+    send_message("⭕️ /online tensvviettat");
+    send_message("⭕️ /allonline tensvviettat");
 }
 if(!$SV or !ServerList($SV)){
-	    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=❎ Vui lòng điền thêm Máy chủ [GÕ] /help để xem các lệnh.");
+	    send_message("❎ Vui lòng điền thêm Máy chủ [GÕ] /help để xem các lệnh.");
 }
 if (strpos($message, "/rsmoc") === 0)
 {
     $return = file_get_contents(ServerList($SV) . "rsmoc.php?sv=" . $CauLenh);
     if ($return == "true")
     {
-        file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=✅ [MỐC] - MÁY CHỦ:" . ServerListName($SV) . " - TÀI KHOẢN:" . $CauLenh . " => đã được Reset.");
+        send_message("✅ [MỐC] - MÁY CHỦ:" . ServerListName($SV) . " - TÀI KHOẢN:" . $CauLenh . " => đã được Reset.");
     }
     else
     {
-        file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=❎ " . $return);
+        send_message("❎ " . $return);
     }
 }
 if (strpos($message, "/online") === 0)
 {
     $curl_scraped_page = file_get_contents(ServerList($SV) . "Online.php?sv=" . $CauLenh);
-    file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=✅ [ONLINE] - MÁY CHỦ:" . ServerListName($SV) . " " . $curl_scraped_page);
+    send_message("✅ [ONLINE] - MÁY CHỦ:" . ServerListName($SV) . " " . $curl_scraped_page);
 }
 if (strpos($message, "/allonline") === 0)
 {
     $curl_scraped_page = file_get_contents(ServerList($SV) . "Onlineall.php");
     if (!$curl_scraped_page or $curl_scraped_page == "true")
     {
-        file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=❎ Câu lệnh thất bại.");
+        send_message("❎ Câu lệnh thất bại.");
     }
     else
     {
         $cachdong = explode('<br/>', $curl_scraped_page);
         for ($i = 0;$i <= count($cachdong);$i++)
         {
-            file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=" . $cachdong[$i]);
+            send_message("" . $cachdong[$i]);
         }
     }
 }
@@ -96,13 +72,38 @@ $LISTITEM = explode(" ", $message)[4];
     $curl_scraped_page = file_get_contents(ServerList($SV) . "sendmailist.php?sv=".$CauLenh."@idnhanvat=".$IDNHANVAT."@list=".$LISTITEM);
     if (!$curl_scraped_page or $curl_scraped_page == "true")
     {
-        file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=✅ Gửi thư thành công.");
+        send_message("✅ Gửi thư thành công.");
     }
     else
     {
-        file_get_contents($path . "/sendmessage?chat_id=" . $chatId . "&text=❎ Gửi thư thất bại.".$curl_scraped_page);
+        send_message("❎ Gửi thư thất bại.".$curl_scraped_page);
     }
 }
-exit();
+///Send Message (Global)
+    function send_message($chat_id,$message_id, $message){
+        $text = urlencode($message);
+        $apiToken = $_ENV['API_TOKEN'];  
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text");
+    }
+    
+//Send Messages with Markdown (Global)
+      function send_MDmessage($chat_id,$message_id, $message){
+        $text = urlencode($message);
+        $apiToken = $_ENV['API_TOKEN'];  
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text&parse_mode=Markdown");
+    }
+///Send Message to Channel
+      function send_Cmessage($channel_id, $message){
+        $text = urlencode($message);
+        $apiToken = $_ENV['API_TOKEN'];
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$channel_id&text=$text");
+    }
+
+//Send Dice (dynamic emoji)
+function sendDice($chat_id,$message_id, $message){
+        $apiToken = $_ENV['API_TOKEN'];  
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendDice?chat_id=$chat_id&reply_to_message_id=$message_id&text=$message");
+    }
+
 
 ?>
